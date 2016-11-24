0b7c4d2d84fd85496f81a9fd7c707d86f6c89604
Mathieu Gouin <mathieugouin@users.noreply.github.com>
2016-11-24 01:16:25 -0500

    Combined get & post.  Added redirect.

--- mailer.py
+++ mailer.py
@@ -5,7 +5,7 @@ import webapp2
 
 
 #*******************************************************************************
-# http://localhost:8080/mailer?from=me&to=you&body=hello&subject=test
+# http://localhost:8080/mailer?from=me@acme.com&to=mgouin@gmail.com&message=hello&subject=test&name=Mat%20Gouin&redirect=redir
 # http://mgouin.appspot.com/mailer?from=me&to=you&body=allo&subject=test
 #
 # NOTES:
@@ -13,14 +13,90 @@ import webapp2
 #*******************************************************************************
 
 def validateInput(args):
-    # TBD test if required minimum argKey (from, to, ...)
-    return True
+    ret = True
+    requiredArgs = [
+        "to",
+        "from"
+    ]
+
+    for a in requiredArgs:
+        if not a in args.keys() or len(args[a]) == 0:
+            ret = False
+            break
+
+    return ret
 
 class MainPage(webapp2.RequestHandler):
 
     #********************************************************************************
     def commonHandler(self):
         mgl.myLog("commonHandler()")
+
+        lines = []
+
+        args = {}
+        for argKey in self.request.arguments():
+            argVal = self.request.get(argKey)
+            #mgl.myLog(argKey + " = " + argVal)
+            args[argKey] = argVal
+            # TBD: Debug
+            lines.append(argKey + " = " + argVal)
+        mgl.myLog(args)
+
+        if not validateInput(args):
+            lines.extend([
+                "Bad attributes: email is not sent",
+                ])
+        # Good attributes
+        else:
+            #Real email sending
+            sendmail.sendMail(
+                self.request.get("from"),
+                self.request.get("to"),
+                self.request.get("subject"),
+                self.request.get("message"),
+                self.request.get("name"),
+                #args["from"], args["to"], args["subject"], args["message"], args["name"]
+            )
+
+            #TBD test
+            #sendmail.sendMailTest()
+
+        redirectURL = self.request.get("redirect").encode('ascii', 'ignore')
+        if len(redirectURL) > 0:
+            mgl.myLog("Performing redirection to " + redirectURL)
+            self.redirect(redirectURL)
+        else:
+            # Normal page display
+            self.response.headers["Content-Type"] = "text/html; charset=utf-8"
+
+            self.response.write('<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"\n')
+            self.response.write('  "http://www.w3.org/TR/html4/loose.dtd">\n')
+            self.response.write('<html>\n' +
+                '<head>\n' +
+                '<title>Mailer</title>\n' +
+                '<meta http-equiv="Content-type" content="text/html;charset=UTF-8">\n' +
+                '</head>\n' +
+                '<body>\n\n')
+
+            for l in lines:
+                #mgl.myLog(l)
+                self.response.write(mgl.processLine(l))
+
+            self.response.write('\n')
+            self.response.write('<div><img alt="Powered by Google" src="powered-by-google-on-white.png" width="104" height="16"></div>\n')
+
+            self.response.write('\n')
+            self.response.write(r"""<script type="text/javascript">""" + "\n")
+            self.response.write(r"""    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){""" + "\n")
+            self.response.write(r"""    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),""" + "\n")
+            self.response.write(r"""    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)""" + "\n")
+            self.response.write(r"""    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');""" + "\n")
+            self.response.write(r"""    ga('create', 'UA-1787000-3', 'mgouin.appspot.com');""" + "\n")
+            self.response.write(r"""    ga('send', 'pageview');""" + "\n")
+            self.response.write(r"""</script>""" + "\n")
+
+            self.response.write("\n</body>\n</html>\n")
 
 
     #********************************************************************************
@@ -29,89 +105,13 @@ class MainPage(webapp2.RequestHandler):
         mgl.myLog("post()")
         self.commonHandler()
 
-        #mgl.myLog(self.request.POST)
-
-        # TBD: get should work
-        # https://cloud.google.com/appengine/docs/python/tools/webapp/requestclass
-
-        #appId = app_identity.get_application_id()
-
-        sendmail.sendMailTest()
-
-        lines = []
-
-        args = {}
-        for argKey in self.request.arguments():
-            argVal = self.request.get(argKey)
-            #mgl.myLog(argKey + " = " + argVal)
-            args[argKey] = argVal
-            lines.append(argKey + " = " + argVal)
-        mgl.myLog(args)
-
-        self.response.content_type = 'text/plain'
-        self.response.write('Email sent.\n')
-        for l in lines:
-            self.response.write(l + "\n")
-
 
     #********************************************************************************
     def get(self):
         mgl.myLog("********************************************************************************")
         mgl.myLog("get()")
-        # TBD not sure...
         self.commonHandler()
 
-        self.response.headers["Content-Type"] = "text/html; charset=utf-8"
-
-        self.response.write('<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"\n')
-        self.response.write('  "http://www.w3.org/TR/html4/loose.dtd">\n')
-        self.response.write('<html>\n' +
-            '<head>\n' +
-            '<title>Mailer</title>\n' +
-            '<meta http-equiv="Content-type" content="text/html;charset=UTF-8">\n' +
-            '</head>\n' +
-            '<body>\n\n')
-
-        lines = []
-
-        args = {}
-        for argKey in self.request.arguments():
-            argVal = self.request.get(argKey)
-            #mgl.myLog(argKey + " = " + argVal)
-            args[argKey] = argVal
-            lines.append(argKey + " = " + argVal)
-        mgl.myLog(args)
-
-        if not validateInput(args):
-            lines.extend([
-                "Bad attributes",
-                ])
-        # Good attributes
-        else:
-            #TBD send mail here...
-            #sendmail.sendMail(args["from"], args["to"], args["subject"], args["message"])
-
-            #TBD test for now...
-            sendmail.sendMailTest()
-
-        for l in lines:
-            #mgl.myLog(l)
-            self.response.write(mgl.processLine(l))
-
-        self.response.write('\n')
-        self.response.write('<div><img alt="Powered by Google" src="powered-by-google-on-white.png" width="104" height="16"></div>\n')
-
-        self.response.write('\n')
-        self.response.write(r"""<script type="text/javascript">""" + "\n")
-        self.response.write(r"""    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){""" + "\n")
-        self.response.write(r"""    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),""" + "\n")
-        self.response.write(r"""    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)""" + "\n")
-        self.response.write(r"""    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');""" + "\n")
-        self.response.write(r"""    ga('create', 'UA-1787000-3', 'mgouin.appspot.com');""" + "\n")
-        self.response.write(r"""    ga('send', 'pageview');""" + "\n")
-        self.response.write(r"""</script>""" + "\n")
-
-        self.response.write("\n</body>\n</html>\n")
 
 app = webapp2.WSGIApplication([(r'/.*', MainPage)], debug=True)
 
--- sendmail.py
+++ sendmail.py
@@ -1,15 +1,20 @@
 # https://cloud.google.com/appengine/docs/python/mail/
 # anything@[APP_NAME].appspotmail.com
 # noreply@mgouin.appspotmail.com
+# "Albert Johnson <Albert.Johnson@example.com>"
 
 from google.appengine.api import mail
 import webapp2
 
+def emailAddName(email, name):
+    if len(name) > 0:
+        email = name + " <" + email + ">"
+    return email
 
-def sendMail(fromAddress, toAddress, subject, body):
+def sendMail(fromAddress, toAddress, subject, body, fromName=""):
     mail.send_mail(
-            sender = "noreply@mgouin.appspotmail.com", # Enforced by google app engine
-            reply_to = fromAddress,
+            sender = emailAddName("noreply@mgouin.appspotmail.com", fromName), # Enforced by google app engine
+            reply_to = emailAddName(fromAddress, fromName),
             to = toAddress,
             bcc = "mgouin@gmail.com",  # for debug purposes
             subject = subject,
@@ -20,9 +25,6 @@ def sendMail(fromAddress, toAddress, subject, body):
     )
 
 def sendMailTest():
-    #         From                       To                  Subject                  Body
-    sendMail("mathieu.gouin@yahoo.com", "mgouin@gmail.com", "test email app engine", "this is the body")
-
-if __name__ == '__main__':
-    sendMailTest()
+    #         From                       To                  Subject                  Body                Name
+    sendMail("mathieu.gouin@yahoo.com", "mgouin@gmail.com", "test email app engine", "this is the body", "Auto Mailer")
 
