73e3a8a9cff2e93b1cb254145e2b2016bd459bb4
Mathieu Gouin <mgouin@gmail.com>
2023-11-06 13:27:56 +0000

    Setup script

---
+++ setup_gcloud.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-453.0.0-linux-x86_64.tar.gz
+tar -xzf *.tar.gz
+./google-cloud-sdk/install.sh
+
+# gcloud init
