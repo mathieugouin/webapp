b7fdb14075852ba267335eee1d258ebe8c81f800
Mathieu Gouin <mgouin@gmail.com>
2023-11-07 19:03:25 +0000

    settings

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import logging
 import flask  # Flask, request, render_template
 
 import mgouinlib as mgl
+# import sendmail
 
 logging.basicConfig(
     level=logging.DEBUG,
@@ -89,6 +90,8 @@ def test_route():
 
 
 if __name__ == '__main__':
+    # sendmail.sendMailTest()
+
     # This is used when running locally only. When deploying to Google App
     # Engine, a webserver process such as Gunicorn will serve the app. This
     # can be configured by adding an `entrypoint` to app.yaml.
